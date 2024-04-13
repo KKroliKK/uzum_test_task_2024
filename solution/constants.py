40824dcf1aa0297a0ff3478d131fa15f99550182
KKroliKK <vagin-andrej02@mail.ru>
2024-04-13 14:55:49 +0300

    data processing draft

---
+++ solution/constants.py
@@ -0,0 +1,10 @@
+import os
+
+
+DATA_PATH = "./data"
+
+PRODUCTS_PATH = os.path.join(DATA_PATH, "products.parquet")
+RETURN_REASONS_PATH = os.path.join(DATA_PATH, "return_reasons.parquet")
+RETURNS_PATH = os.path.join(DATA_PATH, "returns.parquet")
+REVIEWS_PATH = os.path.join(DATA_PATH, "reviews.parquet")
+TEST_PATH = os.path.join(DATA_PATH, "test.parquet")
