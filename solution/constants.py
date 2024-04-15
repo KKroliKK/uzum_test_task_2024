b5eb02929daf99a73c0a9639592dc6763753052d
KKroliKK <vagin-andrej02@mail.ru>
2024-04-15 12:23:34 +0300

    formatting

--- solution/constants.py
+++ solution/constants.py
@@ -15,9 +15,11 @@ REVIEWS_PATH = os.path.join(DATA_PATH, "reviews.parquet")
 TEST_PATH = os.path.join(DATA_PATH, "test.parquet")
 
 LID_176_MODEL_PATH = os.path.join(MODELS_PATH, "lid.176.bin")
-BEST_BASELINE_MODEL = os.path .join(MODELS_PATH, "best_baseline.cbm")
+BEST_BASELINE_MODEL = os.path.join(MODELS_PATH, "best_baseline.cbm")
 
 FASTTEXT_RU_PATH = os.path.join(FASTTEXT_PATH, "cc.ru.300.bin")
 FASTTEXT_UZ_PATH = os.path.join(FASTTEXT_PATH, "cc.uz.300.bin")
 
-BAD_REVIEWS_GROUPED_BY_PRODUCT_ID = os.path.join(EMBEDDINGS_PATH, "bad_reviews_grouped_by_product_id.pkl")
+BAD_REVIEWS_GROUPED_BY_PRODUCT_ID = os.path.join(
+    EMBEDDINGS_PATH, "bad_reviews_grouped_by_product_id.pkl"
+)
